d8ca51d11f1159c03285333e23bb1a087dc87e8f
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-05-12 16:03:09 +0500

    fixes

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Search = ({ onSearchChange }) => {
-  const handleChange = (event) => {
-    onSearchChange(event.target.value);
-  };
-
   return (
-    <div className="mx-4 sm:mx-8 lg:mx-10 mb-6 flex justify-center">
+    <div className="flex justify-center">
       <input
+        type="text"
         placeholder="Search products..."
-        type="search"
-        className="w-full max-w-lg lg:max-w-xl border-2 border-gray-300 p-4 font-medium rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 transition-transform transform hover:scale-105 duration-300"
-        onChange={handleChange}
+        className="border border-gray-400 rounded-lg p-2 w-72"
+        onChange={(e) => onSearchChange(e.target.value)}
       />
     </div>
   );
