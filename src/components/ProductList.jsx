9eb90a6e3027355908e28729ed0d50231f9eec1b
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-05-12 15:51:34 +0500

    changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import ProductCard from "../components/ProductCard";
-import Search from "../components/search"; 
+import Search from "./search"; 
 
 const ProductList = React.memo(({ posts }) => {
   const [searchTerm, setSearchTerm] = useState("");
